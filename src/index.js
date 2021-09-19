57b4c9794b8a200c6164718596c96549ff731e4e
Luna Wen <lunawenresonate@gmail.com>
2021-09-19 19:27:33 +1000

    add prisma client in graphql context

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const { ApolloServer } = require("apollo-server");
 const fs = require("fs");
 const path = require("path");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
 
 let links = [
   {
@@ -57,6 +60,9 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
   resolvers,
+  context: {
+    prisma,
+  },
 });
 
 server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
