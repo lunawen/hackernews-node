5b4f9f04f86cebf6947efbe2d135aee2fe56ea58
Luna Wen <lunawenresonate@gmail.com>
2021-09-19 20:37:07 +1000

    mutation refactoring

--- src/index.js
+++ src/index.js
@@ -4,37 +4,14 @@ const path = require("path");
 const { PrismaClient } = require("@prisma/client");
 const { getUserId } = require("./utils");
 const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
 
 const prisma = new PrismaClient();
 
 // 2 resolver has same structure as typeDefs, type Query => resolver Query
 const resolvers = {
   Query,
-  Mutation: {
-    post: (_, args, context) => {
-      const link = context.prisma.link.create({
-        data: {
-          description: args.description,
-          url: args.url,
-        },
-      });
-      return link;
-    },
-    updateLink: (_, { id, url, description }) => {
-      let toUpdate = links.find((l) => l.id == id);
-      toUpdate.url = url == null ? toUpdate.url : url;
-      toUpdate.description = description == null ? toUpdate.description : description;
-      return toUpdate;
-    },
-    deleteLink: (_, { id }) => {
-      let toDelete = links.filter((l) => l.id == id);
-      if (toDelete.length != 0) {
-        links = links.filter((l) => l.id != id);
-        return true;
-      }
-      return false;
-    },
-  },
+  Mutation,
   Link: {
     id: (parent) => parent.id,
     description: (parent) => parent.description,
