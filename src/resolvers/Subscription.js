e5a28d2cc42aaa0eeca4047d774bc19e3cf895f5
Luna Wen <lunawenresonate@gmail.com>
2021-09-20 21:51:23 +1000

    add subscription resolver

---
+++ src/resolvers/Subscription.js
@@ -0,0 +1,17 @@
+// This function is used to resolve subscriptions and push the event data.
+function newLinkSubscribe(parent, args, context, info) {
+  // subscription return an asyncIterator to push event data
+  return context.pubsub.asyncIterator("NEW_LINK");
+}
+
+const newLink = {
+  subscribe: newLinkSubscribe,
+  // resolve will actually return the data
+  resolve: (payload) => {
+    return payload;
+  },
+};
+
+module.exports = {
+  newLink,
+};
